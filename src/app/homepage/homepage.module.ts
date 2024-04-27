5074fdcd340d8414da7a7159314793051832dace
Joseph Antony <joeavaikath@gmail.com>
2024-04-27 00:52:27 -0400

    Slotted the homepage with components

--- src/app/homepage/homepage.module.ts
+++ src/app/homepage/homepage.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ButtonModule} from 'primeng/button';
 import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, HeaderComponent, FooterComponent, SidebarComponent],
   imports: [
     CommonModule,
     ButtonModule
